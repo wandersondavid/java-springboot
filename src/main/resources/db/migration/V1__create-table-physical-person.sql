4d2d35e1422d3e8bc30529588bdc7bd0ff4a33d5
Wanderson David <wanderson.david@estudante.ifgoiano.edu.br>
2024-05-30 20:53:24 -0300

    refactor: migration

--- src/main/resources/db/migration/V1__create-table-physical-person.sql
+++ src/main/resources/db/migration/V1__create-table-physical-person.sql
@@ -1,17 +1,19 @@
-CREATE TABLE physical_person (
-    id VARCHAR(36) PRIMARY KEY UNIQUE  NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    phone VARCHAR(13) NOT NULL,
-    id_address TEXT NOT NULL,
+CREATE TABLE "physical_person" (
+    "id" VARCHAR(36) PRIMARY KEY UNIQUE  NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "cpf" VARCHAR(11) NOT NULL UNIQUE,
+    "phone" VARCHAR(13) NOT NULL,
+    "id_address" TEXT NOT NULL
 );
 
-CREATE TABLE address (
-    id VARCHAR(36) PRIMARY KEY UNIQUE  NOT NULL,
-    number VARCHAR(10) NOT NULL,
-    complement VARCHAR(255),
-    neighborhood VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    state VARCHAR(2) NOT NULL,
-    zip_code VARCHAR(8) NOT NULL
+CREATE TABLE "address" (
+    "id" VARCHAR(36) PRIMARY KEY UNIQUE  NOT NULL,
+    "number" VARCHAR(10) NOT NULL,
+    "complement" VARCHAR(255),
+    "neighborhood" VARCHAR(255) NOT NULL,
+    "city" VARCHAR(255) NOT NULL,
+    "state" VARCHAR(2) NOT NULL,
+    "zip_code" VARCHAR(8) NOT NULL
 );
+
+ALTER TABLE physical_person ADD CONSTRAINT fk_address FOREIGN KEY (id_address) REFERENCES address(id);
