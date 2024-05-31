050881c9d4bcc38a943d68e7500174965f7234b1
Wanderson David <wanderson.david@estudante.ifgoiano.edu.br>
2024-05-31 00:16:53 -0300

    feat: create controller

---
+++ src/main/java/com/java_springboot/controllers/PhysicalPersonController.java
@@ -0,0 +1,30 @@
+package com.java_springboot.controllers;
+
+import com.java_springboot.domain.person.PhysicalPerson;
+import com.java_springboot.dtos.PhysicalPersonDTO;
+import com.java_springboot.services.PhysicalPersonService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/physical-person")
+public class PhysicalPersonController {
+    private final PhysicalPersonService physicalPersonService;
+
+    public PhysicalPersonController(PhysicalPersonService physicalPersonService) {
+        this.physicalPersonService = physicalPersonService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PhysicalPerson> createPhysicalPerson(@RequestBody PhysicalPersonDTO data) {
+
+        return ResponseEntity.ok(physicalPersonService.createPhysicalPerson(data));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PhysicalPerson>> getAllPhysicalPerson() {
+        return ResponseEntity.ok(physicalPersonService.getAllPhysicalPerson());
+    }
+}
