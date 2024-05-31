d011cb7c170cc86bed99ba8b0e2f84159003e262
Wanderson David <wanderson.david@estudante.ifgoiano.edu.br>
2024-05-30 22:35:23 -0300

    feat: add domin address

---
+++ src/main/java/com/java_springboot/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.java_springboot.domain.address;
+
+import com.java_springboot.domain.person.PhysicalPerson;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="address")
+@Table(name="address")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Address {
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        private String id;
+        private String number;
+        private String complement;
+        private String neighborhood;
+        private String city;
+        private String state;
+        private String zip_code;
+
+        @ManyToOne
+        @JoinColumn(name = "physical_person_id")
+        private PhysicalPerson physical_person;
+}
