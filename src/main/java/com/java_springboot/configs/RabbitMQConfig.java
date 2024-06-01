598d560c6c09d43a55c3535499a53469325b5bb4
Wanderson David <wanderson.david@estudante.ifgoiano.edu.br>
2024-05-31 22:50:13 -0300

    feat: config RabbitMQ add inf queue, exchange and  key to binding queue

--- src/main/java/com/java_springboot/configs/RabbitMQConfig.java
+++ src/main/java/com/java_springboot/configs/RabbitMQConfig.java
@@ -1,18 +1,27 @@
 package com.java_springboot.configs;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
-    @Value("${spring.rabbitmq.queue}")
-    private String queueName;
+    public static final String NOME_FILA = "report-queue";
+    public static final String NOME_EXCHANGE = "report-exchange";
+    public static final String ROUTING_KEY = "report-key";
 
     @Bean
-    public Queue queue() {
-        return new Queue(queueName, true);
+    DirectExchange requestReport() {
+        return new DirectExchange(NOME_EXCHANGE);
+    }
+
+    @Bean
+    Queue queue() {
+        return QueueBuilder.durable(NOME_FILA).build();
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder.bind(queue()).to(requestReport()).with(ROUTING_KEY);
     }
 }
