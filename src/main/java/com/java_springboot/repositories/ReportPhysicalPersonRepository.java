c02d5054d56b019921026ac4b4798411ba538d55
Wanderson David <wanderson.david@estudante.ifgoiano.edu.br>
2024-06-01 23:55:07 -0300

    style: clean code

--- src/main/java/com/java_springboot/repositories/ReportPhysicalPersonRepository.java
+++ src/main/java/com/java_springboot/repositories/ReportPhysicalPersonRepository.java
@@ -1,11 +1,9 @@
 package com.java_springboot.repositories;
 
-import com.java_springboot.domain.person.PhysicalPerson;
 import com.java_springboot.domain.report.person.ReportPhysicalPerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ReportPhysicalPersonRepository extends JpaRepository<ReportPhysicalPerson, String> {
     List<ReportPhysicalPerson> findAllByOrderByCreatedAtDesc();
--- src/main/java/com/java_springboot/services/ReportPhysicalPersonService.java
+++ src/main/java/com/java_springboot/services/ReportPhysicalPersonService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
